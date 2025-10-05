32bc95bfbebe8be59b205a3706980d145382f639
Bilgin Esme <d_besme@borusan.com>
2025-10-05 14:13:24 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'momentum-map-app',
+  webDir: 'www'
+};
+
+export default config;
